8819f525e42a382d9bf358133e085d125a07470c
WhoIsHere <shannonhu519@gmail.com>
2023-09-18 23:08:03 -0700

    add someation footer and contact info.

--- src/pages/Home/Footer.js
+++ src/pages/Home/Footer.js
@@ -37,6 +37,10 @@ const Footer = () => {
 					<img src={tw1} alt="tw1" className="tw1" /> 
 				</a>
       		</div>
+			<div className="footer-info">
+				© Someation <br/>
+				Contact: info@someation.com
+      		</div>
       	</div>
     );
 };
